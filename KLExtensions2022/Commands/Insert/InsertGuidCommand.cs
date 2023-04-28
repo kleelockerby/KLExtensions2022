7ea6c43fa320f074504ab61c00ecc7a588033311
Kevin Lockerby <klockerby@rand.com>
2023-04-28 09:49:11 -0400

    Completed Insert Guid

--- KLExtensions2022/Commands/Insert/InsertGuidCommand.cs
+++ KLExtensions2022/Commands/Insert/InsertGuidCommand.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.CodeDom.Compiler;
 using System.ComponentModel.Design;
+using KLExtensions2022.Commands;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,34 +12,21 @@ using Task = System.Threading.Tasks.Task;
 
 namespace KLExtensions2022
 {
-    internal sealed class InsertGuidCommand
+    [Command(PackageGuids.guidPackageEditContextCmdSetString, PackageIds.InsertGuidCommandId)]
+    internal class InsertGuidCommand : BaseCommand<InsertGuidCommand>
     {
-        private static AsyncPackage package;
-        public static InsertGuidCommand Instance { get; private set; }
-        public static DTE DTE { get; private set; }
-
-        private InsertGuidCommand(OleMenuCommandService commandService)
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            var menuCommandID = new CommandID(PackageGuids.guidPackageEditContextCmdSet, PackageIds.InsertGuidCommandId);
-            var command = new OleMenuCommand(Execute, menuCommandID);
-            commandService.AddCommand(command);
-        }
-
-        public static async Task InitializeAsync(AsyncPackage Package)
-        {
-            package = Package;
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-            DTE = KLExtensions2022Package.DTE as DTE;
-
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new InsertGuidCommand(commandService);
-        }
-
-        private void Execute(object sender, EventArgs e)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            EnvDTE.TextSelection ts = DTE.ActiveDocument.Selection as EnvDTE.TextSelection;
-            ts.Text = System.Guid.NewGuid().ToString();
+            try
+            {
+                await KLExtensions2022Package.JoinTaskFactory.SwitchToMainThreadAsync();
+                EnvDTE.TextSelection ts = DTE.ActiveDocument.Selection as EnvDTE.TextSelection;
+                ts.Text = System.Guid.NewGuid().ToString();
+            }
+            catch(Exception ex)
+            {
+                Logger.Log(ex);
+            }
         }
 
     }
